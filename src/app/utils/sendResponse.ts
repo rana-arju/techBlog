b9598b833990ebb32bf864a4511af2eda23b0963
rana-arju <arjurana20@gmail.com>
2024-12-18 21:34:02 +0600

    sendResponse dynamic function design for response

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,19 @@
+import { Response } from 'express';
+
+const sendResponse = <T>(
+  res: Response,
+  data: {
+    success: boolean;
+    statusCode: number;
+    message?: string;
+    data: T;
+  },
+) => {
+  res.status(data.statusCode).json({
+    message: data.message,
+    data: data.data,
+    success: data.success,
+  });
+};
+
+export default sendResponse;
